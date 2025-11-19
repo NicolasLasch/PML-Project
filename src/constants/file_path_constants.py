ba9b7308afed54949b111f89cf769cfb34e33c72
Doubleyou <akk5922@gmail.com>
2025-11-20 00:24:53 +0800

    File Path Constants

---
+++ src/constants/file_path_constants.py
@@ -0,0 +1,15 @@
+# Raw and Processed path
+PROCESSED_FILE_PATH = 'data/processed/processed_bergen_merged.csv'
+FE_FILE_PATH = 'data/processed/feature_engineered_bergen_merged.csv'
+# Output file paths
+MC_FILE_PATH = 'output/model_comparison_results.csv'
+FI_FILE_PATH = 'output/feature_importance.csv'
+PRED_FILE_PATH = 'output/predictions.csv'
+# Charts
+OUTPUT_CHARTS_FOLDER = 'output/eda_charts'
+MC_CHART = 'model_comparison_results_chart.png'
+FI_CHART = 'feature_importance_chart.png'
+PRED_CHART = 'prediction_results_chart.png'
+HOURLY_PATTERN_CHART = 'hourly_patterns_chart.png'
+WEATHER_CORR_HEATMAP = 'weather_correlation_heatmap.png'
+SEASONAL_PATTERN_CHART = 'seasonal_patterns_chart.png'
