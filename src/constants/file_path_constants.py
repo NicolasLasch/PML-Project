258a4251096fd82a04ab0c9c182bee196f3315b4
Doubleyou <akk5922@gmail.com>
2025-12-03 09:54:45 +0800

    Updated fp constants

--- src/constants/file_path_constants.py
+++ src/constants/file_path_constants.py
@@ -1,13 +1,13 @@
 # Raw and Processed path
-PROCESSED_FILE_PATH = 'data/processed/processed_bergen_merged.csv'
-FE_FILE_PATH = 'data/processed/feature_engineered_bergen_merged.csv'
-SAMPLE_FEATURES_FILE_PATH = 'data/processed/sample_features.csv'
+PROCESSED_FILE_PATH = '../data/processed/processed_bergen_merged.csv'
+FE_FILE_PATH = '../data/processed/feature_engineered_bergen_merged.csv'
+SAMPLE_FEATURES_FILE_PATH = '../data/processed/sample_features.csv'
 # Output file paths
-MC_FILE_PATH = 'output/model_comparison_results.csv'
-FI_FILE_PATH = 'output/feature_importance.csv'
-PRED_FILE_PATH = 'output/predictions.csv'
+MC_FILE_PATH = '../output/model_comparison_results.csv'
+FI_FILE_PATH = '../output/feature_importance.csv'
+PRED_FILE_PATH = '../output/predictions.csv'
 # Charts
-OUTPUT_CHARTS_FOLDER = 'output/eda_charts'
+OUTPUT_CHARTS_FOLDER = '../output/eda_charts'
 MC_CHART = 'model_comparison_results_chart.png'
 FI_CHART = 'feature_importance_chart.png'
 PRED_CHART = 'prediction_results_chart.png'
